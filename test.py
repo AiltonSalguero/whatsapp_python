5b8f3de5e23d103b597a1a4c5400fa6bcfafc868
kyssnar <ailton.salg@gmail.com>
2020-04-30 10:42:17 -0500

    ignored

--- test.py
+++
@@ -1,3 +0,0 @@
-
-lista_mensajes_pechochita = [
-    "Me gusta mucho mirarte", "Eres el motivo de mi sonrisa", "Tu locura es mi ciencia", "Me gusta caminar de la mano contigo", "Tus cachetitos me encantan", "Ya quiero pelar el pollo jajaj", "Por tu culpa me gusta nox XD", "Tus ojitos me enamoran", "Me fascina cuando das tus salittos uwu", "Por ti me amanezco y te preparo tu juguito de fresa de desayuno", "Gracias por ser tan linda conmigo", "Me enamoras todos los dias", "Por ti iria a gamarra otra vez XD", "Aun molesto pienso en ti amorchito"]
